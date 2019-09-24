a7371ac89a152e33b09f4dea862f55c2bcaaebe7
League Student <league@iMac-24.attlocal.net>
2019-09-23 17:00:25 -0700

    RobotGraffiti , MorningZombie , KnockKnock , Greeter , EveryThingIsAwesome

---
+++ src/_03_morning_zombie/MorningZombie.java
@@ -0,0 +1,14 @@
+package _03_morning_zombie;
+
+import javax.swing.JOptionPane;
+
+public class MorningZombie {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null,"Good morning!");
+	JOptionPane.showMessageDialog(null,"First, watch 10 hours of TV.");
+	JOptionPane.showMessageDialog(null,"Next, eat your breakfast slowly while watching TV.");
+	JOptionPane.showMessageDialog(null,"Now, get dressed slowly while watching TV.");
+	JOptionPane.showMessageDialog(null,"Finally make your parents finish getting you ready.");
+	JOptionPane.showMessageDialog(null,"GOOD JOB!!!");
+}
+}
