2f9d7abe2e842635bee6c3a868c250a53ad116d2
cfredberg <53453343+cfredberg@users.noreply.github.com>
2019-09-23 19:05:04 -0700

    Create School.java

---
+++ src/home_projects/School.java
@@ -0,0 +1,11 @@
+package home_projects;
+
+import javax.swing.JOptionPane;
+
+public class School {
+public static void main(String[] args) {
+	JOptionPane.showMessageDialog(null, "Welcome to the Java Learning tool!");
+	String tour = JOptionPane.showInputDialog("Would you like a tour? (Do not use capital letters in your response please)");
+
+}
+}
