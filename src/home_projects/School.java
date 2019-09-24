dc95fc4b96a90384855d245c4d8508448eace2a5
cfredberg <53453343+cfredberg@users.noreply.github.com>
2019-09-24 07:01:56 -0700

    Update School.java

--- src/home_projects/School.java
+++ src/home_projects/School.java
@@ -7,18 +7,29 @@ public static void main(String[] args) {
 	JOptionPane.showMessageDialog(null, "Welcome to the Java Learning tool!");
 	tour();
 }
-public static void school() {
-	String subject = JOptionPane.showInputDialog("Would you like to do math");
+public static void mathOrReading() {
+	String subject = JOptionPane.showInputDialog("Would you like to do math or reading?");
+	if (subject.equalsIgnoreCase("math")) {
+		math();
+	}if (subject.equalsIgnoreCase("reading")) {
+		reading();
+	}
 }
 public static void tour() {
 	String tour = JOptionPane.showInputDialog("Would you like a tour?");
 	if (tour.equalsIgnoreCase("yes")) {
-		school();
+		mathOrReading();
 	}if(tour.equalsIgnoreCase("no")){
-		school();
+		mathOrReading();
 	}else {
 		JOptionPane.showMessageDialog(null, "This is a yes or no question. Plese try again.");
 		tour();
 	}
+}
+public static void math() {
+	
+}
+public static void reading() {
+	
 }
 }
