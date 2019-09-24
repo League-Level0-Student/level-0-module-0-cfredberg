576ea127c2dec74e96b8fc744207778fa208e5af
cfredberg <53453343+cfredberg@users.noreply.github.com>
2019-09-23 20:24:04 -0700

    Update School.java

--- src/home_projects/School.java
+++ src/home_projects/School.java
@@ -5,7 +5,20 @@ import javax.swing.JOptionPane;
 public class School {
 public static void main(String[] args) {
 	JOptionPane.showMessageDialog(null, "Welcome to the Java Learning tool!");
-	String tour = JOptionPane.showInputDialog("Would you like a tour? (Do not use capital letters in your response please)");
-
+	tour();
+}
+public static void school() {
+	String subject = JOptionPane.showInputDialog("Would you like to do math");
+}
+public static void tour() {
+	String tour = JOptionPane.showInputDialog("Would you like a tour?");
+	if (tour.equalsIgnoreCase("yes")) {
+		school();
+	}if(tour.equalsIgnoreCase("no")){
+		school();
+	}else {
+		JOptionPane.showMessageDialog(null, "This is a yes or no question. Plese try again.");
+		tour();
+	}
 }
 }
