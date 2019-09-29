8d5b0503fab0cffcdb3b4f682f8f235c2fc29c3b
cfredberg <53453343+cfredberg@users.noreply.github.com>
2019-09-28 17:36:05 -0700

    Update RobotGraffiti.java

--- src/_02_robot_graffiti/RobotGraffiti.java
+++ src/_02_robot_graffiti/RobotGraffiti.java
@@ -3,21 +3,21 @@ package _02_robot_graffiti;
 import org.jointheleague.graphical.robot.Robot;
 
 public class RobotGraffiti {
-public static void main(String[] args) {
-	Robot robo = new Robot();
-	robo.setSpeed(70);
-	robo.turn(90);
-	robo.move(-300);
-	robo.turn(-90);
-	robo.move(200);
-	robo.turn(230);
-	robo.penDown();
-	robo.setPenColor(255, 0, 0);
-	robo.move(50);
-	robo.turn(-50);
-	robo.move(200);
-	robo.turn(-30);
-	robo.move(50);
-	robo.hide();
-}
+	public static void main(String[] args) {
+		Robot robo = new Robot();
+		robo.setSpeed(70);
+		robo.turn(90);
+		robo.move(-300);
+		robo.turn(-90);
+		robo.move(200);
+		robo.turn(230);
+		robo.penDown();
+		robo.setPenColor(255, 0, 0);
+		robo.move(50);
+		robo.turn(-50);
+		robo.move(200);
+		robo.turn(-30);
+		robo.move(50);
+		robo.hide();
+	}
 }
